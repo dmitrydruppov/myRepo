fce2ab56b1c4162860c9c604a3fb8ccab8a3dc3e
Dmytro <Dmytro_Druppov@epam.com>
2016-10-09 23:55:55 +0300

    logic to print argument

--- my-app/src/main/java/com/mycompany/app/HelloWorldApp.java
+++ my-app/src/main/java/com/mycompany/app/HelloWorldApp.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+		if(args.length > 0) {
+			System.out.println( "Argument :" + args[0] );
+		} else {
+			System.out.println( "Hello World!" );
+		}
     }
 }
